21f9f3709d503bfea05b865bb69252da7147635e
deeptigupta222 <deeptig22@navgurukul.org>
2023-06-08 12:20:53 +0530

    switch case in js

---
+++ script.js
@@ -0,0 +1,18 @@
+const grade="k";
+
+switch (grade){
+    case "A":
+        console.log("A -> very good");
+        // break;
+    case "B":
+        console.log("B -> Good,keep learning");
+        // break;
+    case "C":
+        console.log("C -> need improvement");
+        // break;
+    case "D":
+        console.log("D -> not good");
+        break;
+    default:
+        console.log("invalid grade")
+}
